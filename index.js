284f9f3581efa6b4b3ecbb9ce2082dce7100ed55
Declan Rodgers <declan.rodgers@unifiedfx.com>
2023-02-27 15:26:07 +0000

    refactored EndpointActions library, ready for action testing

--- index.js
+++ index.js
@@ -1,64 +1,19 @@
-const urlConcat = require('./lib/endpointConcat');
+const endpointActions = require('./lib/EndpointActions');
 const core = require('@actions/core');
 const github = require('@actions/github')
-const axios = require('axios');
 var fs = require('file-system');
 
-//const destinationFilter = core.getInput('destination-filter');
-let apiEndpoint = fs.readFileSync("textfiles/destination.txt").toString();
-let xCommand = fs.readFileSync("textfiles/command.txt").toString();
-let tokenData = fs.readFileSync("textfiles/secret.txt").toString();
-let destinationFilter =  fs.readFileSync("textfiles/destinationFilter.txt").toString();
-let deviceEndpoint = urlConcat.urlConcat(apiEndpoint, destinationFilter);
+// let xCommand //= fs.readFileSync("textfiles/command.txt").toString();
+// let tokenData = fs.readFileSync("textfiles/secret.txt").toString();
+// let destinationFilter =  fs.readFileSync("textfiles/destinationFilter.txt").toString();
 
-console.log(`Endpoint: ${deviceEndpoint} \nCommand: ${xCommand}`);
+//let apiEndpoint = core.getInput('api-endpoint');
+let destinationFilter = core.getInput('destination-filter');
+let tokenData = core.getInput('token-data');
+let xCommand = core.getInput('device-command');
 
-// if (!destinationFilter){
-//     CallEndpoint();
-// } else {
-//     CallEndpointWithFilter(destinationFilter);
-// }
-
-//CallEndpoint();
-
-function CallEndpoint(){
-    let headerConfig = {
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer '+tokenData
-        }
-    };
-    
-    try{
-        axios.get(apiEndpoint, headerConfig).then(response =>{
-            fs.writeFileSync("logs/output.txt", response.data);
-            console.log(`Token recieved:\n`,response.data);
-            //console.log(response.data);
-        }).catch(function(error){
-            if(error.response){
-                fs.writeFileSync("logs/axios-error.txt", error.message);
-                console.log("Axios error data written.");
-            }
-        });
-    } catch (error) {
-        fs.writeFileSync("logs/error.txt", error.stack);
-        console.log("Error data written.");
-    }
-}
-
-function CallEndpointWithFilter(destinationFilter){
-    let headerConfig = {
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer '+tokenData
-        }
-    };
-    
-    try{
-        axios.post(apiEndpoint, headerConfig).then(response =>{
-            console.log(response.data);
-        })
-    } catch (error) {
-        console.log(error.stack);
-    }
+if (xCommand === undefined){
+    endpointActions.SendGetCommand(destinationFilter, tokenData);
+} else{
+    endpointActions.SendPostCommand(destinationFilter, tokenData, xCommand);
 }
