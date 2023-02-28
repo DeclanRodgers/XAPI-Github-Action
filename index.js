34a80ee3e88ed09fc6f1f77bcdbfd56f8f090dd5
Declan Rodgers <declan.rodgers@unifiedfx.com>
2023-02-28 11:15:04 +0000

    update axios version

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const core = require('@actions/core');
 const github = require('@actions/github')
 var fs = require('file-system');
 
-let xCommand = fs.readFileSync("textfiles/command.txt").toString();
-let tokenData = fs.readFileSync("textfiles/secret.txt").toString();
-let destinationFilter =  fs.readFileSync("textfiles/destinationFilter.txt").toString();
+// let xCommand = fs.readFileSync("textfiles/command.txt").toString();
+// let tokenData = fs.readFileSync("textfiles/secret.txt").toString();
+// let destinationFilter =  fs.readFileSync("textfiles/destinationFilter.txt").toString();
 
 //let apiEndpoint = core.getInput('api-endpoint');
-// let destinationFilter = core.getInput('destination-filter');
-// let tokenData = core.getInput('token-data');
-// let xCommand = core.getInput('device-command');
+let destinationFilter = core.getInput('destination-filter');
+let tokenData = core.getInput('token-data');
+let xCommand = core.getInput('device-command');
 
 if (xCommand === undefined){
     endpointActions.SendGetCommand(destinationFilter, tokenData);
