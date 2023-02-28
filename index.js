0a60bad38b257f5e78da42a5629e45bd2280c101
Declan Rodgers <declan.rodgers@unifiedfx.com>
2023-02-28 12:49:53 +0000

    node_modules removal

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const core = require('@actions/core');
 const github = require('@actions/github')
 var fs = require('file-system');
 
-// let xCommand //= fs.readFileSync("textfiles/command.txt").toString();
+//let xCommand //= fs.readFileSync("textfiles/command.txt").toString();
 // let tokenData = fs.readFileSync("textfiles/secret.txt").toString();
 // let destinationFilter =  fs.readFileSync("textfiles/destinationFilter.txt").toString();
 
@@ -12,8 +12,8 @@ let destinationFilter = core.getInput('destination-filter');
 let tokenData = core.getInput('token-data');
 let xCommand = core.getInput('device-command');
 
-if (xCommand === undefined){
-    endpointActions.SendGetCommand(destinationFilter, tokenData);
+if (typeof xCommand !== 'undefined'){
+    endpointActions.SendPostCommand(destinationFilter, tokenData, xCommand);    
 } else{
-    endpointActions.SendPostCommand(destinationFilter, tokenData, xCommand);
+    endpointActions.SendGetCommand(destinationFilter, tokenData);
 }
