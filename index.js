88985f24f4cd2871b95db656a03b023f85dd1d7f
Declan Rodgers <declan.rodgers@unifiedfx.com>
2023-02-28 10:41:09 +0000

    case sensitive refactoring

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const endpointActions = require('./lib/EndpointActions');
+const endpointActions = require('./lib/endpointactions');
 const core = require('@actions/core');
 const github = require('@actions/github')
 var fs = require('file-system');
 
-// let xCommand //= fs.readFileSync("textfiles/command.txt").toString();
-// let tokenData = fs.readFileSync("textfiles/secret.txt").toString();
-// let destinationFilter =  fs.readFileSync("textfiles/destinationFilter.txt").toString();
+let xCommand = fs.readFileSync("textfiles/command.txt").toString();
+let tokenData = fs.readFileSync("textfiles/secret.txt").toString();
+let destinationFilter =  fs.readFileSync("textfiles/destinationFilter.txt").toString();
 
 //let apiEndpoint = core.getInput('api-endpoint');
-let destinationFilter = core.getInput('destination-filter');
-let tokenData = core.getInput('token-data');
-let xCommand = core.getInput('device-command');
+// let destinationFilter = core.getInput('destination-filter');
+// let tokenData = core.getInput('token-data');
+// let xCommand = core.getInput('device-command');
 
 if (xCommand === undefined){
     endpointActions.SendGetCommand(destinationFilter, tokenData);
