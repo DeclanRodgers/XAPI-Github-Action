5c479ed344179a99ed7ff4c7ebffefc90694c9e3
Declan Rodgers <declan.rodgers@unifiedfx.com>
2023-02-28 16:37:58 +0000

    refactoring out old code

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const endpointActions = require('./endpointactions');
 const core = require('@actions/core');
 const github = require('@actions/github')
-const fs = require('file-system');
-
-// let xCommand //= fs.readFileSync("textfiles/command.txt").toString();
-// let tokenData = fs.readFileSync("textfiles/secret.txt").toString();
-// let destinationFilter =  fs.readFileSync("textfiles/destinationFilter.txt").toString();
 
 //let apiEndpoint = core.getInput('api-endpoint');
 let destinationFilter = core.getInput('destination-filter');
 let xCommand = core.getInput('device-command');
 let tokenData = core.getInput('token-data');
 
-//if (typeof xCommand !== 'undefined'|| xCommand != null){
 if (xCommand){
     console.log(`Device:${destinationFilter}\nCommand:${xCommand}\n`);
     endpointActions.SendPostCommand(destinationFilter, tokenData, xCommand);    
